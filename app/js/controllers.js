57d59c6414cdd42e663b2f670562fbe13b3ebb29
Marcin Nawrocki <nawrockim@op.pl>
2014-06-14 10:45:41 +0200

    Added ngTable 4 all projects page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -50,4 +50,28 @@ mod.controller('MasterCtrl', ['$scope', '$location', function($scope, $location)
   		}
   	];
 
-  }]);
+  }])
+  .controller('AllProjectsCtrl', ['$scope','ngTableParams', function($scope, ngTableParams) {
+
+    $scope.projectsTableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+            name: 'asc'     // initial sorting
+        }
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                                $filter('orderBy')(data, params.orderBy()) :
+                                data;
+
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
+
+  }]);;
+
+
+  
